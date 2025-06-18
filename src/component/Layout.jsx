eec20d23e55e26250383eff445545648a772fb1c
unknown <borshadevi27@gmail.com>
2025-06-18 23:46:48 +0600

    create 3 file Navbar , Banner , Footer

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -1,7 +1,14 @@
+import Banner from "./Banner";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
 const Layout=({children})=>{
     return(
         <div>
+        <Navbar></Navbar>
+        <Banner></Banner>
          {children}
+         <Footer></Footer>
         </div>
     )
 }
