4baf2daa8d2a4c370206bf5e13c84f95267baabb
unknown <borshadevi27@gmail.com>
2025-06-22 00:47:49 +0600

    added text

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Bannar from "@/component/Banner";
 import Card from "@/component/Card";
+import GameCarossel from "@/component/GameCarossel";
 import ImageBannar from "@/component/ImageBannar";
 
 
 export default function Home() {
   return (
     <div>
+      <GameCarossel></GameCarossel>
       <Card></Card>
       <ImageBannar></ImageBannar>
     </div>
---
+++ src/component/GameCarossel.jsx
@@ -0,0 +1,37 @@
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+const GameCarossel=()=>{
+    return(
+        <div className="mt-10 w-full ml-10">
+           <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm text-black"
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-3xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+        </div>
+    )
+}
+export default GameCarossel;
