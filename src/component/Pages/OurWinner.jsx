7ce24ed85562722f715203ce21772d00f3e29f9b
unknown <borshadevi27@gmail.com>
2025-06-30 13:29:56 +0600

    added text

--- src/component/Pages/OurWinner.jsx
+++ src/component/Pages/OurWinner.jsx
@@ -11,20 +11,25 @@ const OurWinner=()=>{
                 <div className="space-y-3 text-black">
                     <h6 className="font-extralight text-xl">Our Winner</h6>
                     <p className="text-gray-500">A life-changing $1,020,000 win has just turned dreams into reality! Congrats to our lucky winner—proof that <br></br> big rewards are real, and you could be the next success story</p>
-                    <div className="grid grid-cols-3 gap-10">
+                    <div className="grid md:grid-cols-3 grid-cols-1 gap-10">
                        {data.map(da =>  <Card key={da.id} className='w-full max-w-sm'>
-                            <img src={da.image} alt='' className=" h-48 flex justify-center items-center rounded-md"></img>
+                            <img src={da.image} alt='' className="md:h-48  flex justify-center items-center rounded-md"></img>
                             
-                               <div className='font-extralight bg-[#f1f1f1] p-3'>
-                                 Name:{da.name}
-                                <br></br>
-                                Location:{da.location}
-                                <br></br>
-                                Date:{da.date}
-                                <br></br>
-                                Game:{da.game}
-                                <br></br>
-                                Price:{da.price}
+                               <div className='font-extralight bg-[#f1f1f1] flex justify-around text-xl md:p-3 '>
+                                 <div>
+                                    <h6>Name:</h6>
+                                <p>Location:</p>
+                                <p>Date:</p>
+                                <p>Game:</p>
+                                <p>Price:</p>
+                                 </div>
+                                 <div>
+                                    <h6>{da.name}</h6>
+                                <p>{da.location}</p>
+                                <p>{da.date}</p>
+                                <p>{da.game}</p>
+                                <p>{da.price}</p>
+                                 </div>
                                </div>
                            
                         </Card>)}
