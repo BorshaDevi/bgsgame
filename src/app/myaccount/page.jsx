5b7a7b7195ff0edabf1530625fc4d6e5ef31b5c0
unknown <borshadevi27@gmail.com>
2025-07-02 23:44:34 +0600

    added text

---
+++ src/app/myaccount/page.jsx
@@ -0,0 +1,11 @@
+import Account from "@/component/Pages/Account";
+
+
+const page=()=>{
+    return(
+        <>
+       <Account></Account>
+        </>
+    )
+}
+export default page;
--- src/component/Pages/Account.jsx
+++ src/component/Pages/Account.jsx
@@ -1,6 +1,12 @@
 const Account=()=>{
     return(
         <div className="w-full min-h-screen mt-10">
+            <div>
+                <h6 className="text-black font-extralight text-xl">My Account</h6>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     )
 }
