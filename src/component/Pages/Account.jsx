71f3b0f1b37435a62b4a29baa62100d64f67abb7
unknown <borshadevi27@gmail.com>
2025-07-03 23:24:08 +0600

    added text

--- src/component/Pages/Account.jsx
+++ src/component/Pages/Account.jsx
@@ -3,8 +3,13 @@ const Account=()=>{
         <div className="w-full min-h-screen mt-10">
             <div>
                 <h6 className="text-black font-extralight text-xl">My Account</h6>
-                <div>
-                    
+                <div className="flex">
+                    <div>
+                        my account
+                    </div>
+                    <div>
+                        data
+                    </div>
                 </div>
             </div>
         </div>
