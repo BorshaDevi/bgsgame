2744aed13d1a4cec1299ca974edc23f89c588754
unknown <borshadevi27@gmail.com>
2025-07-07 11:38:47 +0600

    added text

--- src/component/Tabs.jsx
+++ src/component/Tabs.jsx
@@ -4,7 +4,7 @@ import 'react-tabs/style/react-tabs.css';
 const TabsCom=()=>{
     return(
         <>
-        <div className="w-full h-full max-w-2xl mx-auto bg-white p-4 rounded shadow-md rounded-md">
+        <div className="w-full h-full max-w-2xl mx-auto bg-white p-4  shadow-md rounded-md">
       <Tabs>
         <TabList>
           <Tab>Entries</Tab>
@@ -14,39 +14,40 @@ const TabsCom=()=>{
 
         <TabPanel>
           <div className="mt-4">
-            <div className='flex justify-between'>
-                <h6 className="text-lg font-semibold mb-2">Lottery</h6>
-            <h6 className="text-lg font-semibold mb-2">Game</h6>
+            <div className='grid grid-cols-2 gap-4 bg-gray-300'>
+                <h6 className="text-lg ">Lottery</h6>
+            <h6 className="text-lg ">Game</h6>
             </div>
-            <div className="flex justify-between bg-gray-100 p-2 rounded">
+            <div className="grid grid-cols-2  p-2 rounded">
               <span>14 June 16:10</span>
               <span>Japan - Power Lottery</span>
-              <span>$0.00</span>
+             
             </div>
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="mt-4">
-            <div className='flex justify-between'>
-                <h6 className="text-lg font-semibold mb-2">Date</h6>
-            <h6 className="text-lg font-semibold mb-2">Lottery</h6>
-            <h6 className="text-lg font-semibold mb-2">Amount</h6>
+            <div className='grid grid-cols-3 gap-4 bg-gray-300'>
+                <h6 className="text-lg ">Date</h6>
+            <h6 className="text-lg ">Lottery</h6>
+            <h6 className="text-lg ">Amount</h6>
             </div>
-            <div className="flex justify-between bg-gray-100 p-2 rounded">
+            <div className="grid grid-cols-3  p-2 rounded">
               <span>14 June 16:10</span>
               <span>$0.00</span>
+              <span>$0.00</span>
             </div>
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="mt-4">
-            <div className='flex justify-between'>
-                <h6 className="text-lg font-semibold mb-2">Date</h6>
-            <h6 className="text-lg font-semibold mb-2">Amount</h6>
+            <div className='grid grid-cols-2 gap-4 bg-gray-300'>
+                <h6 className="text-lg ">Date</h6>
+            <h6 className="text-lg ">Amount</h6>
             </div>
-            <div className="flex justify-between bg-gray-100 p-2 rounded">
+            <div className="grid grid-cols-2 p-2 rounded">
               <span>14 June 16:10</span>
               <span>$0.00</span>
             </div>
