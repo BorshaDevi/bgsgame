96033bd77670ad32f6e1f72c90369b507aaac6cb
unknown <borshadevi27@gmail.com>
2025-07-07 15:24:16 +0600

    added text

--- src/component/Pages/PPolicy.jsx
+++ src/component/Pages/PPolicy.jsx
@@ -41,6 +41,178 @@ const PPolicy=()=>{
                     </ul>
                 </div>
              </div>
+             {/* Responsible Betting */}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Responsible Betting*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>Betting should be treated as entertainment, not a source of income</li>
+                        <li>If you feel you’re developing an addiction, we encourage you to seek help immediately.</li>
+                        <li>Users can set personal limits for time and spending (if feature available).</li>
+                        <li> We support responsible gaming and do not promote compulsive betting.</li>
+                    </ul>
+                </div>
+             </div>
+             {/* Cookie Policy*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Cookie Policy*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>Cookies are used to personalize user experience and analyze site traffic.</li>
+                        <li>Cookies help remember login sessions, preferences, and analytics.</li>
+                        <li>You can manage cookie preferences via your browser settings.</li>
+                        <li> By continuing to use our site, you agree to our use of cookies.</li>
+                    </ul>
+                </div>
+             </div>
+             {/* AML Policy (Anti-Money Laundering)*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">AML Policy (Anti-Money Laundering)*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li> We monitor transactions to prevent suspicious activity.</li>
+                        <li> We reserve the right to conduct KYC checks on any user.</li>
+                        <li> Suspicious activities may lead to account suspension or termination.</li>
+                        <li>  Any account suspected of fraudulent or money-laundering activity will be frozen.</li>
+                        <li>  We report suspicious transactions to relevant authorities where applicable.</li>
+                        <li> Funds from unverified sources may be frozen until investigation is complete.</li>
+                    </ul>
+                </div>
+             </div>
+             {/* Bonuses & Promotions*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Bonuses & Promotions*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li> We monitor transactions to prevent suspicious activity.</li>
+                        <li> We reserve the right to conduct KYC checks on any user.</li>
+                        <li> Suspicious activities may lead to account suspension or termination.</li>
+                        <li>  Any account suspected of fraudulent or money-laundering activity will be frozen.</li>
+                        <li>  We report suspicious transactions to relevant authorities where applicable.</li>
+                        <li> Funds from unverified sources may be frozen until investigation is complete.</li>
+                    </ul>
+                </div>
+             </div>
+             {/* Game Fairness Policy (Fair Play & RNG)*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Game Fairness Policy (Fair Play & RNG)*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>  All lottery and betting outcomes are generated using secure Random Number Generator (RNG) systems.</li>
+                        <li>  Games are unbiased and not manipulated.</li>
+                        <li>  We do not interfere with draw results or game outcomes.</li>
+                    </ul>
+                </div>
+             </div>
+             {/*Account Suspension Policy*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Account Suspension Policy*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>Accounts may be suspended for policy violations, fraud, incomplete KYC, or suspicious activity</li>
+                        <li>Users will be notified where possible before suspension.</li>
+                        <li>Appeals can be submitted through the support email.</li>
+                    </ul>
+                </div>
+             </div>
+             {/*Legal Disclaimer & Jurisdiction*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Legal Disclaimer & Jurisdiction*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>Our operations are managed from servers in the Netherlands.</li>
+                        <li>Any disputes will be governed by applicable Dutch commercial policies unless overridden by local consumer laws.</li>
+                        <li>Our operations follow all guidelines under Curaçao jurisdiction.</li>
+                        <li>We are not liable for technical issues, financial loss, or misuse of the platform.</li>
+                    </ul>
+                </div>
+             </div>
+             {/*Winnings & Tax Information*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10">Winnings & Tax Information*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>All winnings are paid out in full.</li>
+                        <li>Users are solely responsible for declaring and paying any taxes in their local jurisdictions.</li>
+                        <li>We do not withhold or report tax unless legally required.</li>
+                        
+                    </ul>
+                </div>
+             </div>
+
+             {/* ABOUT Us*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10"> ABOUT Us*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <p>We are an online lottery and betting game provider offering instant draw games and skill-based betting such as Ludo and Spin. Our platform<br></br>
+                    aims to provide a smooth, secure, and engaging gaming experience. Though we currently operate without a license, we strive to maintain<br></br> 
+                    transparency and user satisfaction. Our systems are managed from the Netherlands.</p>
+                    <h6 className="font-semibold underline-offset-8 underline">Our Mission*</h6>
+                    <p>To deliver a fun, fair, and responsible online betting experience while prioritizing user security, privacy, and entertainment.</p>
+                    <h6 className="font-semibold underline-offset-8 underline">Contact Us*</h6>
+                    <p>Email: [support@yourwebsite.com](mailto:support@yourwebsite.com)</p>
+                    <h6 className="font-semibold underline-offset-8 underline">Partner With Us (Affiliates)*</h6>
+                    <p>Interested in becoming an affiliate? Contact our partnership team at: [affiliates@yourwebsite.com](mailto:affiliates@yourwebsite.com)</p>
+                </div>
+             </div>
+             {/* INFORMATION*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10"> INFORMATION*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <p>Accepted Payment Methods*</p>
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>We accept payments and withdrawals via Skrill and Cryptocurrency.</li>
+                        <li>All transactions are processed securely.</li>
+                        <li>Minimum and maximum limits may apply for deposits and withdrawals.</li>
+                        <li>Withdrawals are typically processed instantly but may take up to 24 hours depending on the network.</li>
+                        
+                    </ul>
+                </div>
+             </div>
+             {/* FAQ*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10"> FAQ*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <div>
+                        <h6 className="font-semibold">*Q: How do I start playing?*</h6>
+                    <p>A: Register an account, deposit funds via Skill or crypto, choose a game, and begin</p>
+                    </div>
+                    <div>
+                        <h6 className="font-semibold">*Q: How do I withdraw my winnings?*</h6>
+                    <p>A: Go to your wallet and request withdrawal via Skill or crypto.</p>
+                    </div>
+                    <div>
+                        <h6 className="font-semibold">*Q: Why do I need a VPN?*</h6>
+                    <p>A: Our site is geo-restricted and must be accessed via VPN for security and privacy.</p>
+                    </div>
+                    <div>
+                        <h6 className="font-semibold">*Q: Are the games fair?*</h6>
+                    <p>A: All games are luck-based and function through transparent random draw mechanics.</p>
+                    </div>
+                </div>
+             </div>
+             {/* 3. Betting Rules*/}
+             <div className="text-start text-black space-y-4">
+                <h4 className="font-semibold underline-offset-8 underline mt-10"> *3.Betting Rules*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>All bets are final once confirmed.</li>
+                        <li>Results are generated via secure and random systems</li>
+                        <li>Users must comply with game-specific rules provided within each section.</li>
+                    </ul>
+                </div>
+             </div>
+             {/* 4. Account Verification (KYC Process)*/}
+             <div className="text-start text-black space-y-4 mb-5">
+                <h4 className="font-semibold underline-offset-8 underline mt-10"> *4. Account Verification (KYC Process)*</h4>
+                <div className="w-full h-full bg-white rounded-md p-5 text-start shadow space-y-5">
+                    <ul className="list-disc pl-5 space-y-5">
+                        <li>Users may be required to verify identity through government-issued ID.</li>
+                        <li>Payment verification may be requested for large transactions.</li>
+                        <li>Incomplete or fraudulent verification may lead to account suspension.</li>
+                    </ul>
+                </div>
+             </div>
                </div>
              </div>
         </div>
