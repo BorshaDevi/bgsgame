524ec204ececf59ef7aa2263e71b1365e298437b
unknown <borshadevi27@gmail.com>
2025-07-09 12:27:49 +0600

    added swiperjs

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -5,18 +5,69 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import './styles.css';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
 
 const Bannar=()=>{
   return (
     <>
       <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+      <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1750316401/Frame_4_fxvmno.jpg' alt=''/></SwiperSlide>
+        <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1752042308/Group_207_xtjh2r.jpg' alt=''/></SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+export default Bannar;
+
+
+
+
+
+
+// import React, { useRef, useState } from 'react';
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+// import './styles.css';
+
+// // import required modules
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+
+{/* <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
@@ -33,8 +84,4 @@ const Bannar=()=>{
         <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1750316401/Frame_4_fxvmno.jpg' alt=''/></SwiperSlide>
         <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1750316401/Frame_4_fxvmno.jpg' alt=''/></SwiperSlide>
         <SwiperSlide><img src='https://res.cloudinary.com/dg1okcxsx/image/upload/v1750316401/Frame_4_fxvmno.jpg' alt=''/></SwiperSlide>
-      </Swiper>
-    </>
-  );
-}
-export default Bannar;
+      </Swiper> */}
